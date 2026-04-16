03bc87eb7916d8fde4ef379da455ba07162be8e3
Sidd <tahasidd33@gmail.com>
2026-04-17 02:48:44 +0300

    Create RawMaterialRepo.java

---
+++ BD/tack/src/main/java/com/SpicaLabs/tack/repository/RawMaterialRepo.java
@@ -0,0 +1,9 @@
+package com.SpicaLabs.tack.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.SpicaLabs.tack.entity.RawMaterial;
+
+public interface RawMaterialRepo extends JpaRepository<RawMaterial, Long> {
+
+}
