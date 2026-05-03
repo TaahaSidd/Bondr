6e9a44c08345c50958db2249efee0fd55bbc3e90
Sidd <tahasidd33@gmail.com>
2026-05-03 17:16:59 +0530

    Update ProductReqDto.java

--- BD/tack/src/main/java/com/SpicaLabs/tack/dto/request/ProductReqDto.java
+++ BD/tack/src/main/java/com/SpicaLabs/tack/dto/request/ProductReqDto.java
@@ -1,15 +1,28 @@
 package com.SpicaLabs.tack.dto.request;
 
+import com.SpicaLabs.tack.entity.enums.ProductLength;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductReqDto {
 
+    @NotBlank
+    @Size(max = 50, message = "Name too long")
     private String name; //7mm , 11mm etc.
+
+    @NotNull
+    private ProductLength length;
+
+    @NotBlank
     private String description;
 
 }
